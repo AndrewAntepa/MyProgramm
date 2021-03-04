f0a028f304d0edcef0955ddceb5745771f45b686
AndrewAntepa <55337950+AndrewAntepa@users.noreply.github.com>
2021-03-04 11:44:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myprogramm/FirstPage.java
@@ -0,0 +1,22 @@
+package com.example.myprogramm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FirstPage extends Fragment {
+    ListView listView;
+    Fragment fragment;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.first_page, null);
+    }
+}
---
+++ app/src/main/java/com/example/myprogramm/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myprogramm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2, b3, b4;
+    FragmentManager fragmentManager;
+    FirstPage firstPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button1);
+        b2 = findViewById(R.id.button2);
+        b3 = findViewById(R.id.button3);
+        b4 = findViewById(R.id.button4);
+        firstPage = new FirstPage();
+
+        if(savedInstanceState == null){
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(firstPage, null);
+            fragmentTransaction.commit();
+        }
+    }
+}
